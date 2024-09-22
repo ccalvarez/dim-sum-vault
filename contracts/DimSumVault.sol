b52c7411c68a606759023ae78d91303552302996
Z3R0BYT3 <canas360@hotmail.com>
2024-09-22 17:34:50 -0600

    Cambios finales al contrato.

--- contracts/DimSumVault.sol
+++ contracts/DimSumVault.sol
@@ -19,12 +19,17 @@ contract DimSumVault is ERC4626, ReentrancyGuard {
     uint256 public rewardPool; // El pool de recompensas a distribuir
     uint256 public constant FEE_PERCENTAGE = 10; // 10% comision de retiro
     uint256 public constant EARLY_WITHDRAWAL_PENALTY = 10; // 10% penalty por retiro antes de ciclo
-    uint256 public constant MIN_STAKING_DURATION = CYCLE_DURATION; // Minimum staking duration to avoid penalty
+    uint256 private constant CYCLE_DURATION = 7 days; // Duracion del ciclo de staking
     uint256 private totalFees;
     uint256 private lastDistribution; // Ultima distribucion de ganancias
-    uint256 private constant CYCLE_DURATION = 7 days; // Duracion del ciclo de staking
+    
+    struct Deposit {
+        uint256 amount;
+        uint256 timestamp;
+    }
+    
     mapping(address => uint256) public shareHolder; // a mapping that checks if a user has deposited a token.
-    mapping(address => uint256) public depositTimestamps; // Mapping para guardar el timestamp de los depósitos.
+    mapping(address => Deposit[]) public deposits; // Mapping para guardar el timestamp de los depósitos.
 
 
 
@@ -75,6 +80,7 @@ contract DimSumVault is ERC4626, ReentrancyGuard {
       ERC4626(asset) {
         owner = msg.sender;
         paused = false;
+        lastDistribution = block.timestamp;
       }
     
 
@@ -102,16 +108,20 @@ contract DimSumVault is ERC4626, ReentrancyGuard {
 
 
 
-    // Deposit to the vault an ERC20 token
+    // Deposita los tokens ERC20 a la bóveda.
     function stake(uint256 vaultAssets, address vaultReceiver) public whenNotPaused nonReentrant returns (uint256 shares) {
         require(vaultAssets > 0, "Assets must be greater than 0");
         shares = super.deposit(vaultAssets, vaultReceiver);
+        deposits[vaultReceiver].push(Deposit(vaultAssets, block.timestamp));
         shareHolder[vaultReceiver] += shares;
-        depositTimestamps[vaultReceiver] = block.timestamp; // Almacena el timestamp del depósito.
+        // depositTimestamps[vaultReceiver] = block.timestamp; // Almacena el timestamp del depósito.
         return shares;
     }
 
     // Withdraw from the vault the ERC20 tokens deposited
+    // Esta funcion permite el retiro de los fondos depositados.
+    // Si el usuario retira el deposito antes de cumplir el ciclo establecido, se le cobrará una penalizacion
+    // por retiro anticipado.
     function unstake(uint256 vaultAssets, address vaultReceiver, address vaultOwner) public whenNotPaused nonReentrant returns (uint256 shares) {
         require(vaultAssets > 0, "Assets must be greater than 0");
         require(vaultReceiver != address(0), "Receiver must not be address 0");
@@ -120,10 +130,13 @@ contract DimSumVault is ERC4626, ReentrancyGuard {
 
         uint256 fee = (vaultAssets * FEE_PERCENTAGE) / 100;
         uint256 penalty = 0;
-        //Revisa si el retiro sucede antes del MIN_STAKING_DURATION
-        if (block.timestamp < depositTimestamps[vaultReceiver] + MIN_STAKING_DURATION){ // Revisa si el retiro sucede antes del MIN_STAKING_DURATION
-            penalty = (vaultAssets * EARLY_WITHDRAWAL_PENALTY)/100;  // Si es así, se le agrega un penalty de 10% a los shares
+        if (deposits[vaultReceiver].length > 0 ){
+            Deposit memory firstDeposit = deposits[vaultReceiver][0];
+            if (block.timestamp < firstDeposit.timestamp + CYCLE_DURATION){ // Revisa si el retiro sucede antes del MIN_STAKING_DURATION
+                penalty = (vaultAssets * EARLY_WITHDRAWAL_PENALTY) / 100;  // Si es así, se le agrega un penalty de 10% a los shares
+            }
         }
+
         totalFees += fee + penalty;
         rewardPool += (totalFees / 2);
         shareHolder[vaultOwner] -= vaultAssets;
@@ -131,33 +144,30 @@ contract DimSumVault is ERC4626, ReentrancyGuard {
         return shares; // cantidad de shares que fueron quemados
 
     }
-
+    // Funcion para el reclamo de las recompensas del ciclo.
     function vaultRedeem(uint256 vaultShares, address vaultReceiver, address vaultOwner) public whenNotPaused returns (uint256 assets) {
+        Deposit memory firstDeposit = deposits[vaultReceiver][0];
         require(vaultShares > 0, "VaultShares must be greater than 0");
         require(vaultReceiver != address(0), "Receiver must not be address 0");
+        require(firstDeposit.timestamp + CYCLE_DURATION <= block.timestamp, "Deposit not met the time for rewards");
         uint256 vaultAssets = convertToAssets(vaultShares); // Convierte shares a assets
         
         // Calcula el fee (10%)
             uint256 fee = (vaultAssets * FEE_PERCENTAGE) / 100;
-        // Inicializa el penalty a un default de 0.
-            uint256 penalty = 0;
-        //Revisa si el retiro sucede antes del MIN_STAKING_DURATION
-        if (block.timestamp < depositTimestamps[vaultReceiver] + MIN_STAKING_DURATION) {// Revisa si el retiro sucede antes del MIN_STAKING_DURATION
-            penalty = (vaultAssets * EARLY_WITHDRAWAL_PENALTY)/100;  // Si es así, se le agrega un penalty de 10% a los shares
-        }
         // Agrega el fee a totalFees y al rewardPool
-            totalFees += fee + penalty;
+            totalFees += fee;
             rewardPool += (totalFees / 2); // Add half of the total fees to the rewardPool
         // Deducción del fee de los shares del owner y el total de la transacción de los vaultShares
         shareHolder[vaultOwner] -= vaultShares; 
         // Redime los shares.
         assets = super.redeem(vaultShares, vaultReceiver, vaultOwner); 
-        assets = assets - fee - penalty;  // Deduce el fee de los assets
+        assets = assets - fee;  // Deduce el fee de los assets
         return assets;
     }
     
 
-
+    // Funcion de distribucion de recompensas a los usuarios.
+    // Las recompensas se distribuyen en el token de la bóveda.
     function distributeEarnings() public onlyOwner nonReentrant {
         require(block.timestamp >= lastDistribution + CYCLE_DURATION, "El ciclo de staking no ha terminado");
         uint256 earnings = rewardPool;
@@ -200,6 +210,17 @@ contract DimSumVault is ERC4626, ReentrancyGuard {
     // Función para revisar el monto actual del reward pool.
     function getRewardPool() public view returns (uint256) {
         return rewardPool;
+    }
+
+    function getTimeUntilRedeem(address holder) public view returns (uint256){
+        require(deposits[holder].length > 0, "No deposits found for user");
+        Deposit memory firstDeposit = deposit[holder][0];
+        uint256 redeemTime = firstDeposit.timestamp + CYCLE_DURATION;
+        if (block.timestamp < redeemTime){
+            return redeemTime - block.timestamp;
+        } else {
+            return 0;
+        }
     }
 
 
